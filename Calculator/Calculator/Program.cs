ac7f9b20d7b0fc54c67c164e51ed5ae60761d503
SindhuAedla <sindhureddyadla1605@gmail.com>
2023-02-05 17:47:36 -0700

    commited changes

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter input1 ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter input2");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int c = a + b;
+int d = a - b;
+int e = a * b;
+int f = a / b;
+int g = a % b;
+
+Console.WriteLine("Addition = " + c);
+Console.WriteLine("Subtraction = " + d);
+Console.WriteLine("multiplication = " + e);
+Console.WriteLine("Divison = " + f);
+Console.WriteLine("Modular = " + g);
